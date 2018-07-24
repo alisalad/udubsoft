d36fbf0fcee0cc906fc9d34227fd6cd540d56695
Ali Bashi <alisalad123@gmail.com>
2018-07-25 03:34:47 +1000

    Updated services page af Soomaali

--- src/pages/services.js
+++ src/pages/services.js
@@ -4,8 +4,11 @@ import Link from 'gatsby-link'
 const ServicesPage = () => (
   <div>
     <h1>Services</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente culpa vero expedita dolor, consectetur nemo doloribus ex hic maxime earum quae, sit voluptate sint. Saepe soluta vel quisquam dignissimos beatae dolorem, tenetur illum eligendi! Quos perferendis soluta asperiores optio ipsa.</p>
-    <Link to="/">Go back to the homepage</Link>
+    <p>Halkaan waxaad ka haleysaa Dhamaan macluumaadka Udub Software Solutions kuu sameyn doonta. Marwaliba waxaan gacmo furan kusoo dhaweyn doonaa macaamiisheena
+
+    Mahadsanidiin.
+    </p>
+    
   </div>
 )
 
