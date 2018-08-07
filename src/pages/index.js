3b7cbb273171d25b5493f3a1552dd665cc0e8436
Ali Bashi <alisalad123@gmail.com>
2018-08-08 02:23:04 +1000

    waxbaan ka bedaly index file-ka

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,9 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Welcome to my website</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente culpa vero expedita dolor, consectetur nemo doloribus ex hic maxime earum quae, sit voluptate sint. Saepe soluta vel quisquam dignissimos beatae dolorem, tenetur illum eligendi! Quos perferendis soluta asperiores optio ipsa.</p>
-    
+    <p>
+      Website kaan waxaa lagu sameeyay Gatsby, oo React Framework. waan tijaabo
+    </p>
   </div>
 )
 
