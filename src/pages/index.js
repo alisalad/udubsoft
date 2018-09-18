493a6f7cc2b404e17d07e7f17f3ac5d231284fc8
Ali Bashi <alisalad123@gmail.com>
2018-09-19 02:26:29 +1000

    h3

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Welcome to Udub Software Solutions</h1>
     <p>
-      Website kaan waxaa lagu sameeyay <h1>Gatsbyjs v1,</h1> Gatsbjs waa React
+      Website kaan waxaa lagu sameeyay <h3>Gatsbyjs v1,</h3> Gatsbjs waa React
       framework. waxbaan ka bedeli doonaa dhawaan. Haduu Alla idmo.
     </p>
   </div>
