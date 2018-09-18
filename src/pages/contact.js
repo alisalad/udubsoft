93cfd1c51372f2fc5f3ee5b1cca9854c3211fcfb
Ali Bashi <alisalad123@gmail.com>
2018-09-18 23:55:26 +1000

    line break

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,8 +6,8 @@ const ContactPage = () => (
     <h1>Nala soo xiriir</h1>
     <p>
       Waxii fariin ah ama cabasho waxaad noogu soo diri kartaan e-mailka ah
-      info@udub.com ama alisalad123@gmail.com
-      <h1>Mahadsanidiin</h1>
+      info@udub.com ama alisalad123@gmail.com <br />
+      <h2>Mahadsanidiin</h2>
     </p>
     <Link to="/">Go back</Link>
   </div>
